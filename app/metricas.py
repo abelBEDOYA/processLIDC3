6293e4db6322947fe3dd37193626a73e9897ba65
abelBEDOYA <abelgonzalezbernad@gmail.com>
2023-07-31 00:02:29 +0200

    bug solucionado de metricas

--- app/metricas.py
+++ app/metricas.py
@@ -30,8 +30,10 @@ def get_confusion_matrix(id_patient, model, threshold = 0.5, batch = 10):
             if np.all(mask[i:i+batch-1] == 0):
                 print('batch_saltado')
                 continue
+            appendar = mask[i:i+batch-1]
         except:
-            pass
+            appendar = mask[i:]
+
         slices = (i, i+batch-1)
         # print(i+batch, n_slices)
         pred = patient.predict(model, slices=slices, scaled=True, gpu = True)
@@ -39,7 +41,7 @@ def get_confusion_matrix(id_patient, model, threshold = 0.5, batch = 10):
         pred_bin = np.where(pred >= threshold, 1, 0)[:,0,:,:]
         prediccion = np.concatenate((prediccion, pred_bin), axis=0)
         # print(f'{i}', prediccion.shape)
-        masks_slices = np.concatenate(masks_slices, mask[i:i+batch-1])
+        masks_slices = np.concatenate(masks_slices, appendar)
         print(masks_slices.shape, prediccion.shape)
 
     # label = mask[slices[0]: slices[-1]+1].flatten()
